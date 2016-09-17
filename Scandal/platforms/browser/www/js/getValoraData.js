b93085085670f058a65b4110e909d4da6f01f272
JE <jakob.edding@parkspot.xyz>
2016-09-17 02:54:43 +0200

    Solution with fixed Scandit-Input-JSON, call Valora without for-loop

--- Scandal/platforms/browser/www/js/getValoraData.js
+++ Scandal/platforms/browser/www/js/getValoraData.js
@@ -1,12 +1,19 @@
 USER = 'HackZurich';
 PW = 'mKw%VY<7.Yb8D!G-';
 
-function getProductInfo() {
+function searchForProducts() {
+  var jsonString = '{"store-id": "18406", "products": ["9002975301268", "7610469295645", "2050000719073", "5937", "2050000771606"],  "total": "20.53"}'; //as given to me by Emanuel / Scandit
+  console.log(jsonString);
+
+  getProductInfo("18406", "2050000771606");
+}
+
+function getProductInfo(storeId, productId) {
   document.getElementById("infoGoesHere").innerHTML = "wow this works";
 
   var request = new XMLHttpRequest();
 
-  request.open("GET", "https://backend.scango.ch/api/v01/items/find-by-ean/?ean=9002975301268&retail_store_id=18406&format=json", true);
+  request.open("GET", "https://backend.scango.ch/api/v01/items/find-by-ean/?ean=" + productId + "&retail_store_id=" + storeId + "&format=json", true);
 
   request.onreadystatechange = function() {
     console.log("Response received, readyState: " + request.readyState + " status: " + request.status);
