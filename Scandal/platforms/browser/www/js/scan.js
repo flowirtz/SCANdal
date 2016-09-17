5e7bb58eff60534ad5cf14f27626df38cee1bfbd
JE <jakob.edding@parkspot.xyz>
2016-09-17 23:48:14 +0200

    2nd try maps image header

---
+++ Scandal/platforms/browser/www/js/scan.js
@@ -0,0 +1,24 @@
+
+function scanning(){
+
+	Scandit.License.setAppKey("ptxGMCdm5Miis76+JubgFmEULaZUmWbdajgz9zTFn88");
+
+	var settings = new Scandit.ScanSettings();
+	settings.setSymbologyEnabled(Scandit.Barcode.Symbology.QR, true);
+
+	var picker = new Scandit.BarcodePicker(settings);
+	
+	picker.show(success, null, failure);
+	picker.startScanning();
+	console.log("Moin");
+}
+
+function success(session){
+	var receiptData = session.newlyRecognizedCodes[0].data;
+	searchForProducts(JSON.parse(receiptData.replace(/\'/g, '\"')));
+}
+
+function failure(error){
+	alert("error");
+}
+
---
+++ Scandal/platforms/browser/www/plugins/com.mirasense.scanditsdk.plugin/src/constraints.js
@@ -0,0 +1,6 @@
+cordova.define("com.mirasense.scanditsdk.plugin.Constraints", function(require, exports, module) { 
+function Constraints() {
+}
+
+module.exports = Constraints;
+});
