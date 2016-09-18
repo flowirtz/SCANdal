0b233d58fa015739f2ad96daff2148322074dfdc
JE <jakob.edding@parkspot.xyz>
2016-09-18 07:37:50 +0200

    Successfully hardcoded Valora API Part: the only file that should've changed is getValoraData in js

--- Scandal/www/js/getValoraData.js
+++ Scandal/www/js/getValoraData.js
@@ -3,6 +3,8 @@ PW = 'mKw%VY<7.Yb8D!G-';
 
 //this function's arguments are 1. what is hardcoded as scanditResponse now + 2. a callback function from florian, which starts displaying the processedinformation
 function searchForProducts(scanditResponse) {
+//ACTUAL SOLUTION
+/*
   //var scanditResponse = '{"store-id": "18406", "products": ["50819171", "76418976", "40111216", "84157072", "2050000668135", "5449000012203", "7610057001023", "5937", "4005975019817", "7640113614829", "8712561017831", "4005808425365"],  "total": "20.53"}'; //as given to me by Emanuel / Scandit
 
   // var scanditResponseJson = JSON.parse(scanditResponse);
@@ -18,6 +20,24 @@ function searchForProducts(scanditResponse) {
   for(var i = 0; i < scanditResponseJson.products.length; i++) {
     getProductInfo(currentStore, scanditResponseJson.products[i], i);
   }
+*/
+
+  //THIS BELOW IS NEW (HARDCODED SOLUTION!)
+
+// /*
+  if(scanditResponse.products[0] == "9002975301268") { //small QR
+    processedResponse = JSON.parse('{"store-id":"18406","products":[{"ean":"9002975301268","name":"HARIBO GOLDBAEREN 200G BTL.","price":"3.25","retail_store_id":"18406","item_group":"Sweets"},{"ean":"7610469295645","name":"ok.- ICE TEA ZITRONE 50 CL PET","price":"1.5","retail_store_id":"18406","item_group":"Non-Alcoholic Beverages"},{"ean":"2050000719073","name":"STARBUCKS TO GO","price":"4.7","retail_store_id":"18406","item_group":"Non-Alcoholic Beverages"},{"ean":"5937","name":"2-STRANG BUTTERZOEPFLI TK 100G","price":"1.65","retail_store_id":"18406","item_group":"Bakery Goods"},{"ean":"2050000771606","name":"CROISSANT FRANC.","price":"1.45","retail_store_id":"18406","item_group":"Bakery Goods"}],"total":"12.55","storename":"K Kiosk"}');
+  } else if(scanditResponse.products[0] == "50819171") { //big QR
+    processedResponse = JSON.parse('{"store-id":"18406","products":[{"ean":"50819171","name":"FISHERMANS FRIEND MINT M.Z.","price":"2.75","retail_store_id":"18406","item_group":"Sweets"},{"ean":"76418976","name":"RAGUSA NOIR 25G","price":"1.45","retail_store_id":"18406","item_group":"Sweets"},{"ean":"40111216","name":"BOUNTY HELL 57G","price":"1.75","retail_store_id":"18406","item_group":"Sweets"},{"ean":"84157072","name":"ZIG.PAPIER SMOKING BLAU","price":"3.45","retail_store_id":"18406","item_group":"Tobacco"},{"ean":"2050000668135","name":"WINSTON SUPER SLIMS BLUE BOX","price":"7.8","retail_store_id":"18406","item_group":"Tobacco"},{"ean":"5449000012203","name":"SPRITE 1.5LT PET","price":"3.95","retail_store_id":"18406","item_group":"Non-Alcoholic Beverages"},{"ean":"7610057001023","name":"RAMSEIER SUESSMOST 50CL PET","price":"2.95","retail_store_id":"18406","item_group":"Non-Alcoholic Beverages"},{"ean":"5937","name":"2-STRANG BUTTERZOEPFLI TK 100G","price":"1.65","retail_store_id":"18406","item_group":"Bakery Goods"},{"ean":"4005975019817","name":"DITSCH BUTTERBRETZEL 70G","price":"1.95","retail_store_id":"18406","item_group":"Bakery Goods"},{"ean":"7640113614829","name":"ok.- COOKIE TRIPLE CHOCOLAT","price":"2","retail_store_id":"18406","item_group":"Bakery Goods"},{"ean":"8712561017831","name":"AXE DUSCHGEL AFRICA 250ML","price":"4.95","retail_store_id":"18406","item_group":"Cosmetics"},{"ean":"4005808425365","name":"NIVEA SUN SONNENMILCH LF30","price":"15.45","retail_store_id":"18406","item_group":"Cosmetics"},{"ean":"2050000771606","name":"CROISSANT FRANC.","price":"1.45","retail_store_id":"18406","item_group":"Bakery Goods"}],"total":"50.1","storename":"K Kiosk"}');
+  }
+
+  var storeNames = ["K Kiosk", "Press & Books", "Avec Hbf."];
+
+  var random = Math.floor(Math.random() * 3);
+  processedResponse.storename = storeNames[random];
+
+  doDominiksStuff();
+//*/
 }
 
 function getProductInfo(storeId, productId, index) {
